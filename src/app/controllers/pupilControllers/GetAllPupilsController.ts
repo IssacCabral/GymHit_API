823804d030348ea12bb15fc98f0f47572bf16199
henricker <henriquevieira@alu.ufc.br>
2022-06-28 23:34:23 -0300

    fix: ajust query and search options

--- src/app/controllers/pupilControllers/GetAllPupilsController.ts
+++ src/app/controllers/pupilControllers/GetAllPupilsController.ts
@@ -7,7 +7,7 @@ export class GetAllPupilsController{
 
         const service = new GetAllPupilsService()
         const admin_id = request.admin.admin_id
-        const { pupils, totalCounts } = await service.execute(admin_id, Number(page ?? 10), pattern);
+        const { pupils, totalCounts } = await service.execute(admin_id, Number(page ?? 10), pattern as string);
         
         return response.json({ pupils, totalCounts })
     }
